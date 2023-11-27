9aa13ef8d8fbc8df9d538ba2972e87019b0015fe
郑力升 <zhenglisheng.zhengls@bytedance.com>
2023-11-28 02:54:28 +0800

    add msetools rb

---
+++ Formula/msetool.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Msetool < Formula
+  desc "mse tools"
+  homepage "hello"
+  url "https://raw.githubusercontent.com/whalecold/homebrew-tools/main/msetool_0.0.1.tar.gz"
+  sha256 "db60e372639af2a1e88e5d11f13008c3741e3b7d2cc6b2ba57b5416aff57c270"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "msetool"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test msetool`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
